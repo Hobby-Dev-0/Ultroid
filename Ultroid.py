225d74636631681e4be00d1bda2f028b7762e88e
Developer <72003733+InternetAmethyst@users.noreply.github.com>
2021-09-09 17:19:23 +0530

    Fixed 👌🤞

--- Ultroid.py
+++ Ultroid.py
@@ -3,7 +3,7 @@ DEV = os.environ.get("UPSTREAM_REPO", None)
 SED = "Dev" or "DEV" or "dev"
 if DEV == SED:
   import os
-  os.system("git clone -b dev https://github.com/InternetAmethyst/Ultroid.git && cd Ultroid && pip3 install py-Ultroid==62.0b0 && python3 -m pyUltroid")
+  os.system("git clone -b dev https://github.com/InternetAmethyst/Ultroid.git && cd Ultroid && pip3 install -r requirements.txt && python3 -m pyUltroid")
 else:
   import os
   os.system("git clone https://github.com/InternetAmethyst/Ultroid && cd Ultroid && python3 -m pyUltroid")
